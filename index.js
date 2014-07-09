1bae47430955be4cc2516cb6d3a3445b3814237a
luckydrq <drqzju@gmail.com>
2014-07-09 23:35:32 +0800

    add test route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var util = require('util');
 var supportGenerator = require('node-generator-detector')();
 if (!supportGenerator) {
   require('gnode');
@@ -5,10 +6,10 @@ if (!supportGenerator) {
 
 var http = require('http');
 var app = require('./lib/server.js');
-var port = app.get('port') || 7002;
+var port = process.env.NODE_PORT || 7002;
 
 if (!module.parent) {
-  http.createServer(app).listen(port, function(){
-    console.log('Server start listening at %s', port);
+  app.listen(port, function(err) {
+    console.log(util.format('Server start listening at %s', port));
   });
 }
